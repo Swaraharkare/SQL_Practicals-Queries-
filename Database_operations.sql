84e80094d61aac0df41dd434ec9910afb6edc31b
Swaranjali Harkare <168217741+Swaraharkare@users.noreply.github.com>
2026-01-26 21:07:45 +0530

    Add files via upload

---
+++ Database_operations.sql
@@ -0,0 +1,12 @@
+
+--Database creation
+CREATE DATABASE Employee; 
+
+--Drop/Delete database
+DROP DATABASE IF EXISTS Employee;
+
+--Rename database
+ALTER DATABASE Employee MODIFY Name=Employee_Info;
+
+--Switching to database
+USE Employee;
